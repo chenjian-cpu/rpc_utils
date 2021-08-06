841b274e1a49310c7da6bffabf2cc00493bf2e35
lihq1403 <lihaiqing1994@163.com>
2021-08-06 17:04:43 +0800

    获取拆单推送数据

--- src/Contracts/Terp/SplitPurchaseOrderRpcInterface.php
+++ src/Contracts/Terp/SplitPurchaseOrderRpcInterface.php
@@ -13,14 +13,13 @@ use KkErpService\RpcUtils\Structures\TestDTO;
 interface SplitPurchaseOrderRpcInterface
 {
     /**
-     * 获取拆单推送数据
+     * 获取拆单推送数据.
      *
-     * @param string $type 推送系统类型(order-service;k3;)
+     * @param string $type       推送系统类型(order-service;k3;)
      * @param string $createAuth 创建人
-     * @param array $orderType 单据类型(1:分销购销单;2:跨仓调拨单;3:标准销售单;)
-     * @param string $md5 md5
-     * @param int $limit 限制条数
-     * @return array
+     * @param array  $orderType  单据类型(1:分销购销单;2:跨仓调拨单;3:标准销售单;)
+     * @param string $md5        md5
+     * @param int    $limit      限制条数
      */
     public function getPushSplits(string $type, string $createAuth = '', array $orderType = [], string $md5 = '', int $limit = 20): array;
 
