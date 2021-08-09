2f2f3bfff564544b80aa4a9ce1cbd318e3b08c93
lihq1403 <lihaiqing1994@163.com>
2021-08-09 09:37:20 +0800

    增加忽略ids

--- src/Contracts/Terp/SplitPurchaseOrderRpcInterface.php
+++ src/Contracts/Terp/SplitPurchaseOrderRpcInterface.php
@@ -20,8 +20,9 @@ interface SplitPurchaseOrderRpcInterface
      * @param array  $orderType  单据类型(1:分销购销单;2:跨仓调拨单;3:标准销售单;)
      * @param string $md5        md5
      * @param int    $limit      限制条数
+     * @param array  $ignoreIds  忽略的id
      */
-    public function getPushSplits(string $type, string $createAuth = '', array $orderType = [], string $md5 = '', int $limit = 20): array;
+    public function getPushSplits(string $type, string $createAuth = '', array $orderType = [], string $md5 = '', int $limit = 20, array $ignoreIds = []): array;
 
     public function getPushSplitByIdTest(TestParameter $testParameter): TestDTO;
 }
