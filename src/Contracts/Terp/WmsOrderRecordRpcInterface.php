4cfc83f440d1cdc6f6ef8b021299ca1672318da5
lihq1403 <lihaiqing1994@163.com>
2021-08-12 16:40:50 +0800

    增加单独接口查询详情

--- src/Contracts/Terp/WmsOrderRecordRpcInterface.php
+++ src/Contracts/Terp/WmsOrderRecordRpcInterface.php
@@ -12,11 +12,16 @@ interface WmsOrderRecordRpcInterface
     /**
      * 获取需要下推销售出库单数据.
      *
-     * @param string $orderNo   单号
-     * @param bool   $isPushK3  是否推送K3
-     * @param int    $limit     限制条数
-     * @param array  $select    查询字段
-     * @param array  $ignoreIds 忽略的id
+     * @param bool  $isPushK3  是否推送K3
+     * @param int   $limit     限制条数
+     * @param array $select    查询字段
+     * @param array $ignoreIds 忽略的id
      */
-    public function getPushOutStocks(string $orderNo = '', bool $isPushK3 = false, int $limit = 0, array $select = [], array $ignoreIds = []): array;
+    public function getPushOutStocks(bool $isPushK3 = false, int $limit = 0, array $select = [], array $ignoreIds = []): array;
+
+    /**
+     * @param string $orderNo 单号
+     * @param array  $select  查询字段
+     */
+    public function getInfoByOrderNo(string $orderNo = '', array $select = []): array;
 }
