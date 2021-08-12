f1eac0c300828d1c5c57d84d6b339ade13ca5f09
lihq1403 <lihaiqing1994@163.com>
2021-08-12 16:47:15 +0800

    查询字段

--- src/Contracts/Terp/SplitPurchaseOrderRpcInterface.php
+++ src/Contracts/Terp/SplitPurchaseOrderRpcInterface.php
@@ -19,9 +19,10 @@ interface SplitPurchaseOrderRpcInterface
      * @param string $createAuth 创建人
      * @param array  $orderType  单据类型(1:分销购销单;2:跨仓调拨单;3:标准销售单;)
      * @param int    $limit      限制条数
+     * @param array  $select     查询字段
      * @param array  $ignoreIds  忽略的id
      */
-    public function getPushSplits(string $type, string $createAuth = '', array $orderType = [], int $limit = 20, array $ignoreIds = []): array;
+    public function getPushSplits(string $type, string $createAuth = '', array $orderType = [], int $limit = 20, array $select = [], array $ignoreIds = []): array;
 
     /**
      * @param string $md5    md5
