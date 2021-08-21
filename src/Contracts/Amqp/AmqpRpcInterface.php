419d2cbd7e471f51678e6573c2a89eb97a28d756
lihq1403 <lihaiqing1994@163.com>
2021-08-21 17:24:04 +0800

    队列推送

--- src/Contracts/Amqp/AmqpRpcInterface.php
+++ src/Contracts/Amqp/AmqpRpcInterface.php
@@ -16,15 +16,5 @@ use KkErpService\RpcUtils\Structures\Response\CommonResponseDTO;
 
 interface AmqpRpcInterface
 {
-    /**
-     * 队列消息回调.
-     * @param  string $type   类型，关联 producer.配置
-     * @param  string $uri    回调地址
-     * @param  string $method 回调方法
-     * @param  array  $params 回调参数
-     * @return bool   是否成功
-     */
-    public function callback(string $type, string $uri, string $method, array $params): bool;
-
     public function push(AmqpPushRequestDTO $amqpPushRequestDTO): CommonResponseDTO;
 }
