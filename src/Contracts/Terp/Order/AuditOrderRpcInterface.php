9ed7bb789170d9f3a9c3f495ffa1b8ba12e68347
xuyudong@kkguan.com <xuyudong@kkguan.com>
2021-09-14 11:57:48 +0800

    AuditOrderDTO

---
+++ src/Contracts/Terp/Order/AuditOrderRpcInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of the KKGUAN Service.
+ *
+ * (c) KKGUAN Service <>
+ *
+ * 本文件属于KK馆版权所有，泄漏必究。
+ * This file belong to KKGUAN, all rights reserved.
+ */
+namespace KkErpService\RpcUtils\Contracts\Terp\Order;
+
+use KkErpService\RpcUtils\Structures\Request\Terp\Order\AuditOrder\SearchRequestDTO;
+use KkErpService\RpcUtils\Structures\Response\Terp\Order\AuditOrder\SearchResponseDTO;
+
+interface AuditOrderRpcInterface
+{
+    public function search(SearchRequestDTO $searchRequestDTO): SearchResponseDTO;
+}
---
+++ src/Structures/Response/Terp/Order/AuditOrder/AuditOrderDTO.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of the KKGUAN Service.
+ *
+ * (c) KKGUAN Service <>
+ *
+ * 本文件属于KK馆版权所有，泄漏必究。
+ * This file belong to KKGUAN, all rights reserved.
+ */
+namespace KkErpService\RpcUtils\Structures\Response\Terp\Order\AuditOrder;
+
+use KkErpService\RpcUtils\Structures\AbstractDTO;
+
+/**
+ * 审核订单数据.
+ *
+ * @property int    $id         ID
+ * @property int    $orderType  单据类型
+ * @property int    $status     状态
+ * @property int    $errTimes   错误次数
+ * @property string $createTime 创建时间
+ * @property string $createAuth 创建人
+ */
+class AuditOrderDTO extends AbstractDTO
+{
+}
