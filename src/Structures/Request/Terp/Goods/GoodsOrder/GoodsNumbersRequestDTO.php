c882f0490ee7c8f6117aadbb8cd542b03f01848c
lihq1403 <lihaiqing1994@163.com>
2021-09-15 16:00:09 +0800

    通过商品编码查询是否生成形态转换单

---
+++ src/Structures/Request/Terp/Goods/GoodsOrder/GoodsNumbersRequestDTO.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of the KKGUAN Service.
+ *
+ * (c) KKGUAN Service <>
+ *
+ * 本文件属于KK馆版权所有，泄漏必究。
+ * This file belong to KKGUAN, all rights reserved.
+ */
+namespace KkErpService\RpcUtils\Structures\Request\Terp\Goods\GoodsOrder;
+
+use KkErpService\RpcUtils\Structures\AbstractDTO;
+
+/**
+ * 商品编码
+ * @property array $numbers 商品编码
+ */
+class GoodsNumbersRequestDTO extends AbstractDTO
+{
+}
