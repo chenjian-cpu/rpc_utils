fae93238ab4bce6652153572951a2b11c7af1b04
lihq1403 <lihaiqing1994@163.com>
2021-09-17 16:13:08 +0800

    修改协议命名空间，防止其他依赖影响

---
+++ src/Kernel/Component/ProtocolManager/PhpSerializerPacker.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of the KKGUAN Service.
+ *
+ * (c) KKGUAN Service <>
+ *
+ * 本文件属于KK馆版权所有，泄漏必究。
+ * This file belong to KKGUAN, all rights reserved.
+ */
+namespace KkErpService\RpcUtils\Kernel\Component\ProtocolManager;
+
+class PhpSerializerPacker extends \Hyperf\Utils\Packer\PhpSerializerPacker
+{
+}
