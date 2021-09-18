984e39ebe30f35d8c31c7563684f46feb088c823
xuyudong@kkguan.com <xuyudong@kkguan.com>
2021-09-18 12:14:41 +0800

    audit_order枚举

--- src/Constants/Terp/Order/AuditOrder/Status.php
+++ src/Constants/Terp/Order/AuditOrder/Status.php
@@ -2,35 +2,35 @@
 
 declare(strict_types=1);
 /**
- * This file is part of the KKGUAN Service.
+ * This file is part of the KKGUAN Order Service.
  *
- * (c) KKGUAN Service <>
+ * (c) KKGUAN Order Service <>
  *
  * 本文件属于KK馆版权所有，泄漏必究。
  * This file belong to KKGUAN, all rights reserved.
  */
-namespace KkErpService\RpcUtils\Constants\Terp\Order\SplitOrder;
+namespace KkErpService\RpcUtils\Constants\Terp\Order\AuditOrder;
 
 class Status
 {
     //keyword from \Kkguan\ErpService\AuditOrderService
     //审核状态
+
     /**
-     * 未审核
+     * 未审核.
      * @var int
      */
     const STATUS_UNREVIEWED = 0;
 
     /**
-     * 已审核
+     * 已审核.
      * @var int
      */
     const STATUS_AUDITED = 1;
 
     /**
-     * 已提交
+     * 已提交.
      * @var int
      */
     const STATUS_SUBMITTED = 2;
-
 }
