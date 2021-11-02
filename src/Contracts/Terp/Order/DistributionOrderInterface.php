b36fcec701d0fe89a2aec67ac22726c82027981c
Vinchan <ademo@vip.qq.com>
2021-11-02 09:55:47 +0800

    feat: 直接调拨单接入统一推送

---
+++ src/Contracts/Terp/Order/DistributionOrderInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of the KKGUAN Service.
+ *
+ * (c) KKGUAN Service <>
+ *
+ * 本文件属于KK馆版权所有，泄漏必究。
+ * This file belong to KKGUAN, all rights reserved.
+ */
+namespace KkErpService\RpcUtils\Contracts\Terp\Order;
+
+use KkErpService\RpcUtils\Structures\Request\Terp\Order\DistributionOrder\SearchRequestDTO;
+use KkErpService\RpcUtils\Structures\Response\Terp\Order\DistributionOrder\SearchResponseDTO;
+
+/**
+ * 直接调拨单接口.
+ */
+interface DistributionOrderInterface
+{
+    public function search(SearchRequestDTO $searchRequestDTO): SearchResponseDTO;
+}
---
+++ src/Structures/Response/Terp/Order/DistributionOrder/DistributionOrderDTO.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of the KKGUAN Service.
+ *
+ * (c) KKGUAN Service <>
+ *
+ * 本文件属于KK馆版权所有，泄漏必究。
+ * This file belong to KKGUAN, all rights reserved.
+ */
+namespace KkErpService\RpcUtils\Structures\Response\Terp\Order\DistributionOrder;
+
+use KkErpService\RpcUtils\Structures\AbstractDTO;
+
+/**
+ * 直接调拨单单数据.
+ *
+ * @property string $orderNo 单据编号
+ * @property int    $tatus   推送状态
+ */
+class DistributionOrderDTO extends AbstractDTO
+{
+}
